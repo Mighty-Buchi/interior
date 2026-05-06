f01a18e7bb49136bf268a343b209be995b9b63f8
Buchi Ihiaso <ihiasobuchi@gmail.com>
2026-05-06 16:37:15 +0100

    increased logo

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -10,8 +10,8 @@ const Header = () => {
                     <Image
                         src="/next.svg"
                         alt="Logo"
-                        width={40}
-                        height={40 }
+                        width={60}
+                        height={60 }
                     />
                 </Link>
                 <nav>
